db1677f52914d0001f77dc8bf5045add5701906f
taehoon <xogns2134@gmail.com>
2024-03-07 15:12:26 +0900

    feat: 연결 상태 확인 코드 작성

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,30 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function App() {
-  useEffect(() => {
-    let what = axios.get('/api/hello')
-      .then(res => {
-        console.log("successfully accessed")
-      })
-      .then(data => console.log(data))
-      .catch(() => console.log('not found'))
-  });
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        axios.get('/api/hello') //원하는 url작성
+            .then(response => {
+                console.log(response);
+                return response.data; 
+            })
+            .then(message => {
+                setMessage(message);
+                console.log('message',message);
+            })
+            .catch(message => {
+                setMessage('실패');
+            })
+            ;
+    }, [])
+
+    return (
+        <div>
+            <h4>백엔드의 데이터 : {message}</h4>
+        </div>
+    )
+
 }
 
 export default App
