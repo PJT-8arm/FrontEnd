dd5ee9f478985a7063d4a555e6212fec088d867d
geniushee <h1116jeon@gmail.com>
2024-03-14 18:02:11 +0900

    feat : tailwind 및 daisyUI 적용

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,17 @@ import RecruitmentForm from './RecruitmentForm';
 import RecruitmentDetail from './RecruitmentDetail'; // 임포트한 RecruitmentDetail 컴포넌트
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                {/* 메인 페이지로 접근했을 때 보여줄 컴포넌트 */}
-                <Route path="/" element={<RecruitmentForm />} />
-                {/* 상세 페이지의 경로와 매칭될 컴포넌트 */}
-                <Route path="/recruitments/detail/:id" element={<RecruitmentDetail />} />
-                {/* 다른 모든 경로에 대한 처리. 404 페이지나 메인 페이지 리다이렉트 등의 로직을 추가할 수 있습니다. */}
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        {/* 메인 페이지로 접근했을 때 보여줄 컴포넌트 */}
+        <Route path="/" element={<RecruitmentForm />} />
+        {/* 상세 페이지의 경로와 매칭될 컴포넌트 */}
+        <Route path="/recruitments/detail/:id" element={<RecruitmentDetail />} />
+        {/* 다른 모든 경로에 대한 처리. 404 페이지나 메인 페이지 리다이렉트 등의 로직을 추가할 수 있습니다. */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
