d0bdbb3886e46d6f4528551a96826817ba183bd1
YUBEENLIM <148295292+YUBEENLIM@users.noreply.github.com>
2024-03-15 13:43:13 +0900

    6/e/memberJoin (#16)
    * feat:로그인,회원가입 기능
    
    * refactor:일부 코드 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RecruitmentForm from './RecruitmentForm';
-import RecruitmentDetail from './RecruitmentDetail'; // 임포트한 RecruitmentDetail 컴포넌트
+import RecruitmentDetail from './RecruitmentDetail';
+import RegisterPage from './RegisterPage';
+import LoginPage from './LoginPage';
 
 function App() {
     return (
@@ -11,11 +13,14 @@ function App() {
                 <Route path="/" element={<RecruitmentForm />} />
                 {/* 상세 페이지의 경로와 매칭될 컴포넌트 */}
                 <Route path="/recruitments/detail/:id" element={<RecruitmentDetail />} />
+                {/* 회원가입 페이지 */}
+                <Route path="/register" element={<RegisterPage />} />
+                {/* 로그인 페이지 */}
+                <Route path="/login" element={<LoginPage />} />
                 {/* 다른 모든 경로에 대한 처리. 404 페이지나 메인 페이지 리다이렉트 등의 로직을 추가할 수 있습니다. */}
             </Routes>
         </Router>
-    );
+    );        
 }
 
 export default App;
-
